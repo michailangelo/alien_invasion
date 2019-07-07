9b229a38b591c4d046ed24797e1bfddca001cffa
Michail Samartzis <michailangelo@gmail.com>
2019-07-08 00:18:21 +0300

    Initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,20 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+def run_game():
+    pygame.init()
+    ai_settings=Settings()
+    screen =pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+        screen.fill(ai_settings.bg_color)
+        pygame.display.flip()
+
+run_game()
