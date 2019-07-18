1425250cd3e7bb495fd862980459608fd0c31d18
Michail Samartzis <michail.samartzis@eurodyn.com>
2019-07-18 10:20:59 +0300

    Added support for movement of aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,13 +14,16 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # Make a group to store bullets in
     bullets = Group()
-
+    aliens = Group()
+    # Create a fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     # Start the main loop of the game
     while True:
-        gf.check_events(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 1.5;
 
         # bullet settings
         self.bullet_speed_factor = 1
@@ -19,6 +19,12 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
 
 
 
